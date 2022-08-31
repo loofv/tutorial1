fb910e3005254ecff1dd8e17cc6375cf195ac70e
Love Hulterström <love.hulterstrom@benify.com>
2022-08-31 10:11:31 +0200

    Initial Commit

---
+++ tutorial1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial1
+//
+//  Created by Love Hulterström on 2022-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial1/tutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial1App.swift
+//  tutorial1
+//
+//  Created by Love Hulterström on 2022-08-31.
+//
+
+import SwiftUI
+
+@main
+struct tutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
